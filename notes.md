0477edb7f06d6c39a280c0306eaa72583f53e171
Lawrence D'Anna <larry@elder-gods.org>
2021-12-24 11:03:08 -0500

    hack up serial port support

--- notes.md
+++ notes.md
@@ -5,6 +5,7 @@ notes
 `138065` is the unlock command 
 
 `c22a 25` is send data command 
+`c22a 1` tells it to stop sending data
 
 testbench is ldanna@mfma-jessup, COM2 9600 baud
 
--- telem35.py
+++ telem35.py
@@ -1,108 +1,78 @@
-#!/usr/bin/python3 -tt
-# Sierra Telemetry decoder ver 0.5 by N7CK
-# Version for Python 3
-# September 2020
-
-# Get the system and telnet libraries
 
 import sys
-import telnetlib
+import argparse
+import os
+import re
 
-# Clear Screen function
+# pip install pyserial
+from serial import Serial
 
-from os import system, name
+
+def init_sierra(serial):
+  serial.write(b'138065\r\n')  #unlock 
+  serial.write(b'c22a 25\r\n') #enable telemetry
+
+def scan_for_records(serial):
+  buffer = ""
+  while True:
+    if len(buffer) > 2048:
+      buffer = buffer[1024:]
+    buffer += serial.read(1).decode('ascii')
+    m = re.search(r'\[[\da-fA-F ]*\]$', buffer)
+    if not m:
+      m = re.search(r'\{[\da-fA-F ]*\}$', buffer)
+    if not m:
+      continue
+    yield m.group()
+    buffer = ""
 
 def clear():
-# for Windows
-  if name == 'nt':
-    _ = system('cls')
-# For mac and Linux (os.name ="posix")
+  if os.name == 'nt':
+    os.system('cls')
   else:
-    _ = system('clear')
+    os.system('clear')
 
-#
-# This is the input section to set variables.
-#
-clear()
-print ("              Sierra Telemetry Decoder v0.5\n\n")
-print ("Please have the site name, ip address and port ready.\n\n")
+def main():
 
-SITE = input ("Site Name: ")
-if SITE == "":
-  SITE = "Dragoon"
+  parser = argparse.ArgumentParser("read telemetry from Sierra radio controller")
+  parser.add_argument("port")
+  parser.add_argument("baud", type=int)
+  args = parser.parse_args()
 
-HOST = input ("Site ip address or Hostname: ")
-if HOST == "":
-  HOST = "192.168.97.20"
+  serial = Serial(args.port, baudrate=args.baud, timeout=.01)
 
-PORT = input ("Port: ")
-if PORT == "":
-  PORT = "5006"
-
-print ("Trying: Site: "+SITE + "  Host: " + HOST + "  Port: " + PORT)
-print ("Waiting for connection and some telemetry . . .")
-
-# Site info for reference
-#SITE = "Dragoon"
-#HOST = "192.168.97.20"
-#PORT = "5007"
-#SITE = "HELIOGRAPH"
-#HOST = "8.193.12.246"
-#PORT = "5006"
+  init_sierra(serial)
+  for rec in scan_for_records(serial):
+    clear()
+    tcode(rec)
+    sys.stdout.flush()
 
 
-# connect
-telnetObj=telnetlib.Telnet(HOST,PORT)
+def tcode(record):
+  if record[0] != '[':
+    return
 
+  telem = record[1:-1]
 
-# send cr / lf
-message = ("\n\r").encode('ascii')
-
-telnetObj.write(message)
-
-#while  1 == 1:
-def tcode():
-  output1=telnetObj.read_until(b"]")
-  output = output1.decode('ascii')
-  telem = (output)
-  telem = (telem.strip())
-#  print (telem)
-#  print (len(telem))
-# Simulated Incoming data:
-#telem = "[01 06 8F FF 05]"
-
-
-# Get rid of brackets
-  telem = telem [1:15]
-
-# For future referemce Braces:
-#linkon = "00"
-#loop = "00"
-#disabled = "00"
-#subdisabled = "00"
-#split_group = "00"
-#interfaced = "00"
+  # For future referemce Braces:
+  #linkon = "00"
+  #loop = "00"
+  #disabled = "00"
+  #subdisabled = "00"
+  #split_group = "00"
+  #interfaced = "00"
 
   Brackets = ("COR","PL","QuRX","DTMF","PTT")
-  y=0
-  telemlist= [0,0,0,0,0]
-  for x in range (0,15,3):
-    telem1 = telem [x:x+2]
-    y=y+1
-#  print (telem1)
-    telemlist[y-1] = int(telem1,16)
 
-#print (telemlist)
+  telemlist = [int(x, 16) for x in record[1:-1].split()]
 
   telemread = [(0,""),(0,""),(0,""),(0,""),(0,"")]
 
   for x  in range(5):
 #  print (telemlist[x])
     telemread[x] = telemlist[x], Brackets [x]
-#print 9telemread
-# Clear screen
-  clear()
-  print ( "     " + SITE + " Port Status --  Hit ENTER to exit. \n")
+
+
   for y in range(8):
     print ("Port " + str(y)+ "   ", end ='')
 
@@ -119,36 +89,6 @@ def tcode():
 
     print (" ")
 
-# This runs the main decode loop .. waiting for error (ctl-c) - Works on Linux
-#try:
-#  while True:
-#    tcode()
-#except:
-#  pass
 
-
-import threading as th
-
-keep_going = True
-def key_capture_thread():
-  global keep_going
-  input()
-  keep_going = False
-
-def do_stuff():
-  th.Thread(target=key_capture_thread, args=(), name='key_capture_thread', daemon=True).start()
-  while keep_going:
-    tcode()
-
-do_stuff()
-
-# Close telent session, clear screen and exit
-
-telnetObj.close()
-
-clear()
-print ("Telent session finished\n\n")
-
-
-exit()
-
+if __name__ == '__main__':
+  main()
